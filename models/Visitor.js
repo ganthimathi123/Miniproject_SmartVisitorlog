5f578c9ab5ee559ef19430c38e612b8e2d054089
Ganthimathi.V <145111899+ganthimathi123@users.noreply.github.com>
2026-03-24 12:17:55 +0530

    Remove duplicate visitorSchema definition

--- models/Visitor.js
+++ models/Visitor.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const visitorSchema = new mongoose.Schema({
@@ -29,35 +28,3 @@ const visitorSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Visitor', visitorSchema);
-=======
-const mongoose = require('mongoose');
-
-const visitorSchema = new mongoose.Schema({
-  personId: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    default: 'Unknown'
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now
-  },
-  imageUrl: {
-    type: String,
-    default: null
-  },
-  accessGranted: {
-    type: Boolean,
-    default: false
-  },
-  emotionDetected: {
-    type: String,
-    default: null
-  }
-});
-
-module.exports = mongoose.model('Visitor', visitorSchema);
->>>>>>> bef748dd58cc032f2e9c5527e21a8411c0f1eadd
